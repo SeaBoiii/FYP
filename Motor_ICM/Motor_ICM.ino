9f4df8f7d5b3b972db394d30985b28cacf2d014c
SeaBoiii <5379756+SeaBoiii@users.noreply.github.com>
2022-01-11 15:37:57 +0800

    Added both Bokeh and Firework Effect
    Addresses #25

--- Motor_ICM/Motor_ICM.ino
+++ Motor_ICM/Motor_ICM.ino
@@ -615,10 +615,31 @@ void loop() {
     /* Fixed Patterns */ //Todo
     case 6: {
       switch (sscreen) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
+        case 0: { // Bokeh Effect (S: Focus Max, Zoom Current. F: Focus Current, Zoom Widest)
+          printMoveSteps(NULL, string_32, CADETBLUE, 3); // setting lens to starting position
+          moveMotor(FOCUS, focus_range);
+          updateScreen(2000);
+          countdownMenu();
+          goDist(FOCUS, string_32, focus_current, VIOLET, false, motor_time/2);
+          goDist(ZOOM, string_32, 0, VIOLET, true, motor_time/2);
+          sscreen = resetScreen(sscreen);
+          break;
+        }
+        case 1: { // Firework Effect (Focus Max, then min, then return to original)
+          printMoveSteps(NULL, string_33, CADETBLUE, 3);
+          moveMotor(FOCUS, focus_range);
+          updateScreen(2000);
+          countdownMenu();
+          goDist(FOCUS, string_33, 0, AZURE, false, ((float)3/4)*motor_time);
+          goDist(FOCUS, string_33, focus_current, AZURE, false, ((float)1/4)*motor_time);
+          break;
+        }
+        case 2: { // Zoom Blur Effect
+          break;
+        }
+        case 3: { // Sine Wave Effect
+          break;
+        }
         case 4: // back
           screen = -1;
           sscreen = -1;
